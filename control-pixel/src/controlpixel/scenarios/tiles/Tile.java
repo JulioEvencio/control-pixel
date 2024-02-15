5c1c9eff5100346882dd94816008578ffa66266f
JulioEvencio <julioigreja.dev@gmail.com>
2024-02-14 22:00:36 -0300

    added: tile

---
+++ control-pixel/src/controlpixel/scenarios/tiles/Tile.java
@@ -0,0 +1,29 @@
+package controlpixel.scenarios.tiles;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import controlpixel.util.Camera;
+import controlpixel.util.Rect;
+
+public class Tile {
+
+	private final Rect rect;
+	private final Color color;
+
+	public Tile(Rect rect, Color color) {
+		this.rect = rect;
+
+		this.color = color;
+	}
+
+	public Rect getRect() {
+		return this.rect;
+	}
+
+	public void render(Graphics render) {
+		render.setColor(this.color);
+		render.fillRect(this.rect.getX() - Camera.x, this.rect.getY() - Camera.y, this.rect.getWidth(), this.rect.getHeight());
+	}
+
+}
