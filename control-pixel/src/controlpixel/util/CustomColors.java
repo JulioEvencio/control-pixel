f1c6276d432e4f4daa0686616319f323684fadd8
JulioEvencio <julioigreja.dev@gmail.com>
2024-02-21 23:41:37 -0300

    update: custom colors

--- control-pixel/src/controlpixel/util/CustomColors.java
+++ control-pixel/src/controlpixel/util/CustomColors.java
@@ -6,7 +6,5 @@ public class CustomColors {
 
 	public static final Color BLACK = new Color(0, 0, 0);
 	public static final Color BLUE = new Color(0, 153, 219);
-	public static final Color GRAY = new Color(90, 105, 136);
-	public static final Color GRAY_DARK = new Color(80, 80, 80);
 
 }
