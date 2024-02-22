60faad3342b803144f97313d162234ee5a43b07b
JulioEvencio <julioigreja.dev@gmail.com>
2024-02-22 00:44:33 -0300

    added: template for level

--- control-pixel/src/controlpixel/scenarios/levels/Level01.java
+++ control-pixel/src/controlpixel/scenarios/levels/Level01.java
@@ -22,15 +22,19 @@ public class Level01 extends Scenario {
 			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 			{' ', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', ' '},
-			{' ', 'L', 'C', 'R', ' ', 'L', 'C', 'C', 'C', 'C', 'C', 'R', ' ', 'L', 'C', 'C'},
-			{' ', 'F', 'G', 'H', ' ', 'F', 'G', 'G', 'G', 'G', 'G', 'H', ' ', 'F', 'G', 'G'},
-			{'W', 'F', 'G', 'H', 'W', 'F', 'G', 'G', 'G', 'G', 'G', 'H', 'W', 'F', 'G', 'G'},
+			{' ', 'L', 'C', 'R', ' ', 'L', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'},
+			{' ', 'F', 'G', 'H', ' ', 'F', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
+			{'W', 'F', 'G', 'H', 'W', 'F', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
 		};
 	}
 
 	@Override
 	protected void setStrings() {
-		super.texts.add(new TextRender(StringLevel.LEVEL_01.getValue(), 80, 80, Color.WHITE, super.game.getRender()));
+		super.texts.add(new TextRender(StringLevel.LEVEL_01.getValue(), 25, 25, Color.WHITE, super.game.getRender()));
+		super.texts.add(new TextRender(StringLevel.LINE_02.getValue(), 25, 50, Color.WHITE, super.game.getRender()));
+		super.texts.add(new TextRender(StringLevel.LINE_03.getValue(), 25, 75, Color.WHITE, super.game.getRender()));
+		super.texts.add(new TextRender(StringLevel.LINE_04.getValue(), 25, 100, Color.WHITE, super.game.getRender()));
+		super.texts.add(new TextRender(StringLevel.LINE_05.getValue(), 25, 125, Color.WHITE, super.game.getRender()));
 	}
 
 	@Override
--- control-pixel/src/controlpixel/strings/StringLevel.java
+++ control-pixel/src/controlpixel/strings/StringLevel.java
@@ -4,4 +4,10 @@ public class StringLevel {
 
 	public static final GameString LEVEL_01 = new GameString("Level 01");
 
+	public static final GameString LINE_01 = new GameString("Ajude a Pixel, a garota, a chegar até o portal");
+	public static final GameString LINE_02 = new GameString("Use o mouse para adicionar objetos ao mapa");
+	public static final GameString LINE_03 = new GameString("Cada objeto tem uma função diferente");
+	public static final GameString LINE_04 = new GameString("O bloco é um bloco onde a Pixel pode andar");
+	public static final GameString LINE_05 = new GameString("Aperte barra de espaço para sair do modo de construção");
+
 }
